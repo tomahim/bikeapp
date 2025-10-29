7f8dac973052f4e0445400e79cf92e56a44fbced
thimblot <thimblot@gmail.com>
2025-10-29 15:08:54 +0000

    publish on master merge

---
+++ firebase.config.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCldq-oJRG9E84eNH9KCGzanPYGRGP9_zw",
+    authDomain: "bike-app-ccac8.firebaseapp.com",
+    projectId: "bike-app-ccac8",
+    storageBucket: "bike-app-ccac8.firebasestorage.app",
+    messagingSenderId: "806555574329",
+    appId: "1:806555574329:web:4e3e09f2cce91dfd45ade8",
+    measurementId: "G-9DGWWC8HWS"
+  };

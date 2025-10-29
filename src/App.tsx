9f40fcdf20afb0bbdc7ebed401d9fe6aa1ae04a2
thomas <thomas@thomas-spinergie.(none)>
2025-10-29 16:39:31 +0100

    mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/logo.jpg'
+import './App.scss'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [speed, setSpeed] = useState(10.5);
+  const [power, setPower] = useState(150);
+  const [logs, setLogs] = useState(["test log 1", "test log 2"]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="logo">
+        <img alt="logo" src={logo} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="gauges">
+        <b>{speed}</b> km/h - <b>{power}w</b>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="logs">
+        <h2>Logs</h2>
+        {logs.map(log => <div className="log">{log}</div>)}
+      </div>
     </>
   )
 }
